40a0e56656fdd0eca30971fd87f3cd1a0e4e27cb
Amanda Tan <ahkaitan.68@gmail.com>
2019-02-20 02:10:49 +0000

    day 1

--- js/game.js
+++ js/game.js
@@ -1,39 +1,79 @@
 // NOTE - You must have a level with the name "start".  This is used as the first level in the game.
 
 var game = {
-    music: "98_Lost_Mine.mp3",
-    background_image: "intro-bg.jpg",
+    background_image: "",
+    music: "",
     levels: {
 
         start: {
-            message: "You come across a dark cave",
+            message: "You see a blinking bright light in the alleyway",
             choices: [
                 {
-                    text: "Enter the cave",
-                    nextLevel: "cave",
+                    text: "Follow the light",
+                    nextLevel: "room",
                 },
 
                 {
-                    text: "Keep on moving",
+                    text: "Keep on walking",
                     nextLevel: "field",
                 },
             ]
         },
 
-        cave: {
-            background_image: "fire.gif",
-            music: "Final-Fantasy-7-Boss-Battle.mp3",
-            message: "You come across a fire monster or something!",
+        room: {
+            background_image: "",
+            music: "",
+            message: "You find yourself in an abandoned room, the bright light is coming from a screen. But suddenly a loud bang comes from the back room. What do you do?",
             choices: [
                 {
-                    text: "Start over",
-                    nextLevel: "start",
+                    text: "Mess around with the computer",
+                    nextLevel: "sentMessage",
+                },
+                
+                {
+                    text: "Explore the back room",
+                    nextLevel: "backRoom",
                 },
             ]
         },
 
+        sentMessage: {
+            background_image: "",
+            music: "",
+            message: "A frantic replicant appears onscreen. You don't understand it because it's talking so fast. But the last thing it says are some instructions.",
+            choices: [
+                {
+                    text:"Follow instructions",
+                    nextLevel:"",
+                },
+                
+                {
+                    text:"",
+                    nextLevel:"",
+                },
+            ]
+        },
+        
+        backRoom: {
+            background_image: "",
+            music: "",
+            message: "You enter the back room and see a ",
+            choices: [
+                {
+                    text:"",
+                    nextLevel:"",
+                },
+                
+                {
+                    text:"",
+                    nextLevel:"",
+                },
+            ]
+        },
         field: {
-            message: "Some adventurer you are...",
+            background_image: "",
+            music: "",
+            message: "You made it safely home",
             choices: [
                 {
                     text: "Start over",
